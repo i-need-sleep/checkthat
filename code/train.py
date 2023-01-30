15ff3971bccae5048f32ed348921927a9b5a75c7
Will <yh2689@nyu.edu>
2023-01-30 19:50:21 +0400

    sh

--- code/train.py
+++ code/train.py
@@ -85,7 +85,7 @@ def train(args):
         running_loss = 0
 
         # Eval
-        if epoch % 2 == 0:
+        if epoch % 5 == 0:
 
             prec, recall, f1 = eval(model, dev_loader, device)
             writer.add_scalar('dev/prec', prec, n_iter)
@@ -155,7 +155,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--batch_size_dev', default=16, type=int)
 
-    parser.add_argument('--lr', default=2e-6, type=float)
+    parser.add_argument('--lr', default=1e-5, type=float)
     parser.add_argument('--n_epoch', default=1000, type=int)
     parser.add_argument('--checkpoint', default='', type=str) 
 
